a5b231a9c50179c5eb6b5e6db28b1a93e63fbceb
keepAwayFrom404 <xff777@qq.com>
2020-09-25 17:29:31 +0800

    feat(router): 除了功能类，其他接口不加权限

--- server/routes.ts
+++ server/routes.ts
@@ -19,21 +19,21 @@ const protectedRouter = new Router({
   prefix: '/api'
 });
 // users 相关的路由
-protectedRouter.post('/user/list', UserController.listUsers);
-protectedRouter.get('/users/:id', UserController.showUserDetail);
-protectedRouter.put('/users/:id', UserController.updateUser);
-protectedRouter.delete('/users/:id', UserController.deleteUser);
+unprotectedRouter.post('/user/list', UserController.listUsers);
+unprotectedRouter.get('/users/:id', UserController.showUserDetail);
+unprotectedRouter.put('/users/:id', UserController.updateUser);
+unprotectedRouter.delete('/users/:id', UserController.deleteUser);
 
 // article 相关路由
 protectedRouter.post('/article/add', ArticleController.addArticle);
-protectedRouter.post('/article/list', ArticleController.listArticles);
+unprotectedRouter.post('/article/list', ArticleController.listArticles);
 
 // categories 相关路由
-protectedRouter.post('/categories/add', CategoryController.addCategory)
-protectedRouter.post('/categories/list', CategoryController.listCategories)
+unprotectedRouter.post('/categories/add', CategoryController.addCategory)
+unprotectedRouter.post('/categories/list', CategoryController.listCategories)
 
 // week 相关路由
-protectedRouter.post('/weeks/list', WeekController.listWeeks)
-protectedRouter.post('/weeks/add', WeekController.addWeek)
+unprotectedRouter.post('/weeks/list', WeekController.listWeeks)
+unprotectedRouter.post('/weeks/add', WeekController.addWeek)
 
 export { protectedRouter, unprotectedRouter };
